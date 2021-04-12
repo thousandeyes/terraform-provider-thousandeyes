a163f920e18127d8c86ee8b74795368fd88bed96
Joshua Blanchard <joshua.blanchard@gmail.com>
2021-04-12 23:52:46 +0000

    feat(sdk): Update sdk ver to add validation funcs
    ThousandEyes API has cases where the existence or values of certain
    fields are dependent on other fields.  Enhancing schema specification to
    validate such fields requires new validation functions which are only
    available in newer versions of the plugin SDK (ie: ConflictsWith,
    ExactlyOneOf, AtLeastOneOf, RequiredWith).
    
    This would have the consequence of losing support for terraform versions
    below 0.12

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/plugin"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"terraform-provider-thousandeyes/thousandeyes"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() terraform.ResourceProvider {
+		ProviderFunc: func() *schema.Provider {
 			return thousandeyes.Provider()
 		},
 	})
--- thousandeyes/util_test.go
+++ thousandeyes/util_test.go
@@ -4,8 +4,8 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/william20111/go-thousandeyes"
 )
 
