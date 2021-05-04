31ca63cb0ba1316101ed17988364a1cf6ae43c21
Joshua Blanchard <joshua.blanchard@gmail.com>
2021-05-04 17:09:25 +0000

    Remove duplicate schema

--- thousandeyes/schemas.go
+++ thousandeyes/schemas.go
@@ -807,12 +807,6 @@ var schemas = map[string]*schema.Schema{
 		Required:     true,
 		ValidateFunc: validation.StringInSlice([]string{"tests", "agents", "endpoint_tests", "endpoint_agents"}, false),
 	},
-	"type-label": {
-		Type:         schema.TypeString,
-		Description:  "Type of label (tests, agents, or endpoint_agents",
-		Required:     true,
-		ValidateFunc: validation.StringInSlice([]string{"tests", "agents", "endpoint_agents"}, false),
-	},
 	"url": {
 		Type:        schema.TypeString,
 		Description: "target for the test",
