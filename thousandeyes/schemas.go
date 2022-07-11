3504b04e4f58cb96a5e04f03eb2246da633953ca
Raul Benencia <raul@thousandeyes.com>
2022-07-11 16:14:42 -0700

    refactor: remove DNS+ references
    DNS+ tests are no longer supported.

--- thousandeyes/schemas.go
+++ thousandeyes/schemas.go
@@ -260,7 +260,7 @@ var schemas = map[string]*schema.Schema{
 		Description:  "Acceptable test types, verbose names",
 		Type:         schema.TypeString,
 		Required:     true,
-		ValidateFunc: validation.StringInSlice([]string{"Page Load", "HTTP Server", "End-to-End (Server)", "End-to-End (Agent)", "Voice", "DNS+ Domain", "DNS+ Server", "DNS Server", "DNS Trace", "DNSSEC", "Transactions", "Web Transactions", "BGP", "Path Trace", "FTP", "SIP Server"}, false),
+		ValidateFunc: validation.StringInSlice([]string{"Page Load", "HTTP Server", "End-to-End (Server)", "End-to-End (Agent)", "Voice", "DNS Server", "DNS Trace", "DNSSEC", "Transactions", "Web Transactions", "BGP", "Path Trace", "FTP", "SIP Server"}, false),
 	},
 	"alerts_enabled": {
 		Type:        schema.TypeBool,
