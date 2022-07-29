8ec7add44141e2bde851c8342d221b1b46f715a0
Raul Benencia <raul@thousandeyes.com>
2022-07-29 10:39:23 -0700

    docs: update example for data-source account_group

--- examples/data-sources/thousandeyes_account_group/data-source.tf
+++ examples/data-sources/thousandeyes_account_group/data-source.tf
@@ -1,3 +1,19 @@
 data "thousandeyes_account_group" "sre" {
   name = "SRE"
 }
+
+resource "thousandeyes_http_server" "thousandeyes_http_test" {
+  test_name      = "Example HTTP test with account group sharing set from Terraform provider"
+  interval       = 120
+  alerts_enabled = true
+
+  url = "https://www.thousandeyes.com"
+
+  shared_with_accounts  {
+    aid = data.thousandeyes_account_group.sre.aid
+  }
+
+  agents {
+    agent_id = 3 # Singapur
+  }
+}
