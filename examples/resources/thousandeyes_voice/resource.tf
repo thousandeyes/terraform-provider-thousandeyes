5c0e0079892cded363568e4cdf7325305b1648d8
Raul Benencia <raul@thousandeyes.com>
2022-07-29 13:35:24 -0700

    doc: update examples with use_public_bgp

--- examples/resources/thousandeyes_voice/resource.tf
+++ examples/resources/thousandeyes_voice/resource.tf
@@ -1,7 +1,10 @@
 resource "thousandeyes_voice" "example_voice_test" {
-  test_name      = "Example RTP stream test set from Terraform provider"
-  interval       = 120
-  alerts_enabled = false
+  test_name        = "Example RTP stream test set from Terraform provider"
+  interval         = 120
+  alerts_enabled   = false
+
+  bgp_measurements = true
+  use_public_bgp   = true
 
   target_agent_id = 4 # Tokyo
 
