94a7aa4fe0ee5f7652535de5f90391515eef22bc
adchella-te <101803940+adchella-te@users.noreply.github.com>
2022-10-24 10:48:17 +0200

    docs: add resources example

---
+++ examples/resources/thousandeyes_web_transaction/resource.tf
@@ -0,0 +1,24 @@
+resource "thousandeyes_web_transaction" "example_web_transaction_test" {
+  test_name      = "Example Web Transaction test set from Terraform provider"
+  interval      = 120
+  alerts_enabled = false
+  
+  url            = "https://www.thousandeyes.com"
+
+  transaction_script = <<EOF
+  import { By, Key, until } from 'selenium-webdriver'; 
+  import { driver, markers, credentials, downloads, transaction, test } from 'thousandeyes'; 
+  runScript(); 
+  async function runScript() 
+  { const settings = test.getSettings();
+  // Load page
+  await driver.get(settings.url);
+  await driver.wait(until.titleIs('Digital Experience Monitoring | ThousandEyes'), 1000);
+  await driver.takeScreenshot();
+};
+EOF
+
+  agents {
+    agent_id = 3 # Singapur
+  }
+} 
