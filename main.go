21169eb7f0e3c67b7e16bb8272f6cef09aa9a841
João Malés <85952626+joaomper-TE@users.noreply.github.com>
2023-12-13 14:56:17 +0000

    test2

--- main.go
+++ main.go
@@ -11,12 +11,12 @@ import (
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
 func main() {
-	var debugModeTest bool
+	var debugModeTest2 bool
 
-	flag.BoolVar(&debugModeTest, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.BoolVar(&debugModeTest2, "debug", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
 	opts := &plugin.ServeOpts{
-		Debug:        debugModeTest,
+		Debug:        debugModeTest2,
 		ProviderAddr: "registry.terraform.io/thousandeyes/thousandeyes",
 		ProviderFunc: thousandeyes.New(),
 	}
