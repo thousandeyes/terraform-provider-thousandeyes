251dc8c177e2bfd62fefd582e268a66f440551c7
sipatov-te <sipatov@thousandeyes.com>
2025-03-21 11:51:21 +0200

    feat: add user acceptance test for page load resource type (#202)

--- thousandeyes/acceptance_resources/page_load/basic.tf
+++ thousandeyes/acceptance_resources/page_load/basic.tf
@@ -3,17 +3,18 @@ data "thousandeyes_agent" "arg_amsterdam" {
 }
 
 resource "thousandeyes_alert_rule" "alert-rule-http-test" {
-  rule_name                 = "Custom UAT HTTP Alert Rule"
-  alert_type                = "HTTP Server"
-  expression                = "(((responseCode >= 400) || (responseCode == 0)))"
+  rule_name                 = "Custom UAT Page Load Alert Rule"
+  alert_type                = "Page Load"
+  expression                = "((errorCount >= 50) && (pageLoadHasError == true) && (Auto(timeToFirstByte >= Medium sensitivity)) && (Auto(onContentLoadTime >= Medium sensitivity)))"
   rounds_violating_out_of   = 1
   rounds_violating_required = 1
   minimum_sources           = 1
 }
 
-resource "thousandeyes_http_server" "test" {
-  test_name      = "User Acceptance Test - HTTP"
+resource "thousandeyes_page_load" "test" {
+  test_name      = "User Acceptance Test - Page Load"
   interval       = 120
+  http_interval  = 120
   alerts_enabled = true
   url            = "https://www.thousandeyes.com"
 
@@ -22,7 +23,7 @@ resource "thousandeyes_http_server" "test" {
   }
 
   alert_rules {
-    rule_id = 921621 #HTTP Default Alert Rule
+    rule_id = 921620 #Page Load Default Alert Rule
   }
 
   alert_rules {
