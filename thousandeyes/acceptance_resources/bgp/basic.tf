db74a5a4c3589da3ce0f511e4067bf36b1494545
sipatov-te <sipatov@thousandeyes.com>
2025-03-28 01:02:18 +0200

    feat: adjust bgp uat test to api v7

--- thousandeyes/acceptance_resources/bgp/basic.tf
+++ thousandeyes/acceptance_resources/bgp/basic.tf
@@ -1,6 +1,6 @@
 resource "thousandeyes_alert_rule" "test" {
   rule_name                 = "Custom UAT BGP Alert Rule"
-  alert_type                = "BGP"
+  alert_type                = "bgp"
   expression                = "[(((prefixLengthIPv4 >= 16) && (prefixLengthIPv4 <= 32)) || ((prefixLengthIPv6 >= 32) && (prefixLengthIPv6 <= 128)))]((reachability < 100%) && (changes > 1))"
   minimum_sources           = 1
   rounds_violating_required = 1
@@ -12,12 +12,5 @@ resource "thousandeyes_bgp" "test" {
   alerts_enabled = true
   use_public_bgp = true
   prefix         = "192.0.2.0/24"
-
-  alert_rules {
-    rule_id = thousandeyes_alert_rule.test.id
-  }
-
-  alert_rules {
-    rule_id = 921614 #BGP Default Alert Rule
-  }
+  alert_rules    = [thousandeyes_alert_rule.test.id, "921614"]
 }
