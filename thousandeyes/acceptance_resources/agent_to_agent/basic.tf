c2769ce8625b601856dabd5a6740f228ed15e4f9
sipatov-te <sipatov@thousandeyes.com>
2025-04-01 23:38:43 +0300

    fix: agent to agent tf file

--- thousandeyes/acceptance_resources/agent_to_agent/basic.tf
+++ thousandeyes/acceptance_resources/agent_to_agent/basic.tf
@@ -25,14 +25,14 @@ resource "thousandeyes_alert_rule" "alert-rule-agent_to_agent" {
 }
 
 resource "thousandeyes_agent_to_agent" "test" {
-  test_name      = "User Acceptance Test - Aget To Agent"
-  interval       = "120"
-  alerts_enabled = true
-
-  direction       = "bidirectional"
-  protocol        = "tcp"
-  target_agent_id = data.thousandeyes_agent.arg_frankfurt.agent_id
-  agents          = [data.thousandeyes_agent.arg_amsterdam.agent_id]
-  use_public_bgp  = true
-  alert_rules     = [data.thousandeyes_alert_rule.def_alert_rule.id, thousandeyes_alert_rule.alert-rule-agent_to_agent.id]
+  test_name        = "User Acceptance Test - Aget To Agent"
+  interval         = "120"
+  alerts_enabled   = true
+  bgp_measurements = true
+  direction        = "bidirectional"
+  protocol         = "tcp"
+  target_agent_id  = data.thousandeyes_agent.arg_frankfurt.agent_id
+  agents           = [data.thousandeyes_agent.arg_amsterdam.agent_id]
+  use_public_bgp   = true
+  alert_rules      = [data.thousandeyes_alert_rule.def_alert_rule.id, thousandeyes_alert_rule.alert-rule-agent_to_agent.id]
 }
