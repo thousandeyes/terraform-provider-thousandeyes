f3e81ea56fd2147bbabbdf26d7a0f8ec6c93125a
sipatov-te <sipatov@thousandeyes.com>
2025-04-04 19:32:59 +0300

    fix merge

--- thousandeyes/resource_agent_to_agent.go
+++ thousandeyes/resource_agent_to_agent.go
@@ -53,7 +53,7 @@ func resourceAgentAgentUpdate(d *schema.ResourceData, m interface{}) error {
 	api := (*tests.AgentToAgentTestsAPIService)(&apiClient.Common)
 
 	log.Printf("[INFO] Updating ThousandEyes Test %s", d.Id())
-	update := ResourceUpdate(d, &tests.AgentToAgentTestRequest{})
+	update := buildAgentAgentStruct(d)
 
 	req := api.UpdateAgentToAgentTest(d.Id()).AgentToAgentTestRequest(*update).Expand(tests.AllowedExpandTestOptionsEnumValues)
 	req = SetAidFromContext(apiClient.GetConfig().Context, req)
