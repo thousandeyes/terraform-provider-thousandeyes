d7bb4b6605b4b0b5dc474ca8ae374f35f2b663d9
sipatov-te <sipatov@thousandeyes.com>
2025-04-25 18:12:18 +0300

    fieat: update examples according to schema for api v3

--- examples/resources/thousandeyes_dnssec/resource.tf
+++ examples/resources/thousandeyes_dnssec/resource.tf
@@ -2,10 +2,6 @@ resource "thousandeyes_dnssec" "example_dnssec_test" {
   test_name      = "Example DNSSEC test set from Terraform provider"
   interval       = 120
   alerts_enabled = false
-
-  domain = "www.thousandeyes.com ANY"
-
-  agents {
-    agent_id = 3 # Singapore
-  }
+  domain         = "www.thousandeyes.com ANY"
+  agents         = ["3"] # Singapore
 }
--- examples/resources/thousandeyes_ftp_server/resource.tf
+++ examples/resources/thousandeyes_ftp_server/resource.tf
@@ -1,18 +1,11 @@
 resource "thousandeyes_ftp_server" "example_ftp_server_test" {
-  test_name      = "Example FTP Server test set from Terraform provider"
-  interval       = 120
-  alerts_enabled = false
-
-
-  url = "www.thousandeyes.com"
-  request_type = "Download"
-
+  test_name            = "Example FTP Server test set from Terraform provider"
+  interval             = 120
+  alerts_enabled       = false
+  url                  = "www.thousandeyes.com"
+  request_type         = "download"
   network_measurements = false #This flag is set to false to disable bandwidth measurements which is only possible when using Enterprise Agents
-
-  username = "admin"
-  password = "welcome"
-
-  agents {
-    agent_id = 3 # Singapore
-  }
+  username             = "admin"
+  password             = "welcome" #Password won't be saved in state, so Terraform will notify that this field should be updated durring terraform plan
+  agents               = ["3"]     # Singapore
 }
