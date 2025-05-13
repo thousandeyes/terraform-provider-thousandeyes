03fac4033edb88e975ca15c140da3aafe71d9a32
Phellippe Lima <120402907+phpinhei-te@users.noreply.github.com>
2025-05-13 15:35:56 +0100

    fix: fixes docs to improve clarity on the new version feature set (#240)
    * fix: fixes alert rule resource docs example
    
    * fix: gives clarity on provider featureset

--- examples/resources/thousandeyes_alert_rule/resource.tf
+++ examples/resources/thousandeyes_alert_rule/resource.tf
@@ -2,6 +2,7 @@ resource "thousandeyes_alert_rule" "example_alert_rule" {
   rule_name                 = "Example Alert Rule set from Terraform provider"
   alert_type                = "http-server"
   expression                = "((errorType != \"None\"))" # Error Type is ANY
-  rounds_violating_required = 1
-  rounds_violating_out_of   = 1
+  minimum_sources           = 2
+  rounds_violating_required = 4
+  rounds_violating_out_of   = 4
 }
