b25a64ada216220ba0f6be9a1a363e425d6d3e1f
tshe-te <tshe@thousandeyes.com>
2025-10-14 10:50:34 +0100

    change the agent to Frankfurt

--- thousandeyes/acceptance_resources/api/basic.tf
+++ thousandeyes/acceptance_resources/api/basic.tf
@@ -1,5 +1,5 @@
-data "thousandeyes_agent" "arg_amsterdam" {
-  agent_name = "Amsterdam, Netherlands"
+data "thousandeyes_agent" "arg_frankfurt" {
+  agent_name = "Frankfurt, Germany"
 }
 
 data "thousandeyes_alert_rule" "def_alert_rule" {
@@ -20,7 +20,7 @@ resource "thousandeyes_api" "test" {
   interval             = 120
   alerts_enabled       = true
   url                  = "https://www.thousandeyes.com"
-  agents               = [data.thousandeyes_agent.arg_amsterdam.agent_id]
+  agents               = [data.thousandeyes_agent.arg_frankfurt.agent_id]
   alert_rules          = [data.thousandeyes_alert_rule.def_alert_rule.id, thousandeyes_alert_rule.alert-rule-http-test.id]
   network_measurements = false
   bgp_measurements     = false
