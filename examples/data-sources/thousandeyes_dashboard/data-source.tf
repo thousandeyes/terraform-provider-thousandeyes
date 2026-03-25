9d636ace4c8dcebc21eb44c4cc9c176e710376f5
Stu Lowes <stulowes@thousandeyes.com>
2026-03-25 09:33:01 +0000

    CP-3838 Remove dashboard example

--- examples/data-sources/thousandeyes_dashboard/data-source.tf
+++
@@ -1,8 +0,0 @@
-resource "thousandeyes_dashboard" "test_dashboard" {
-  description      = "Test Dashboard with All Widget Types"
-  title            = "Test Dashboard - All Widgets"
-  is_private       = true
-  default_timespan {
-    duration = 7200
-  }
-}
